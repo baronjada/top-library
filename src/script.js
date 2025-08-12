ca7856614b02aa7df673cf21edc25aebf133018d
Jada Baron <jadachantellebaron@gmail.com>
2025-08-12 18:48:58 +0100

    Build addBookToLibrary and displayLibrary functions
    Reworked three book variables to be dynamic within addBookToLibrary function by pushing into global bookLibrary array. Then used DOM manipulation to display book cards within displayLibrary function.

--- src/script.js
+++ src/script.js
@@ -1,18 +1,4 @@
-// function Book(type, title, author, pages, status) {
-//   if (!new.target) {
-//     throw error("You must use the 'new' operator to call the constructor");
-//   }
-
-//   this.element = document.createElement(type);
-//   this.element.innerText = this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.status = status;
-//   this.id = crypto.randomUUID();
-//   this.displayBookInfo = function () {
-//     return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}, Status: ${this.status}`;
-//   };
-// }
+const bookLibrary = [];
 
 function Book(title, author, pages, status) {
   if (!new.target) {
@@ -29,17 +15,41 @@ function Book(title, author, pages, status) {
   };
 }
 
-const book1 = new Book("Before I Let Go", "Kennedy Ryan", 425, "Read");
-const book2 = new Book("Honey & Spice", "Bolu Babalola", 324, "Not Read");
-const book3 = new Book(
+function addBookToLibrary(title, author, pages, status) {
+  const book = new Book(title, author, pages, status);
+
+  bookLibrary.push(book);
+}
+
+function displayLibrary() {
+  for (let i = 0; i < bookLibrary.length; i++) {
+    const bookCard = document.createElement("div");
+    bookCard.setAttribute("data-book-id", bookLibrary[i].id);
+    bookCard.className = "book-card";
+    document.body.appendChild(bookCard);
+
+    const bookTitleElement = document.createElement("p");
+    const bookTitleContent = document.createTextNode(bookLibrary[i].title);
+    bookTitleElement.appendChild(bookTitleContent);
+    bookCard.appendChild(bookTitleElement);
+  }
+}
+
+const book1 = addBookToLibrary("Before I Let Go", "Kennedy Ryan", 425, "Read");
+
+const book2 = addBookToLibrary(
+  "Honey & Spice",
+  "Bolu Babalola",
+  324,
+  "Not Read"
+);
+const book3 = addBookToLibrary(
   "I Will Teach You To Be Rich",
   "Ramit Sethi",
   444,
   "Read"
 );
 
-const bookLibrary = [];
-
-const addBook = bookLibrary.push(book1, book2, book3);
-
 console.log(bookLibrary);
+
+displayLibrary();
