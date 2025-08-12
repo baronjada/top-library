02b116eea7f92bea110fc3514169b315e132de31
Jada Baron <jadachantellebaron@gmail.com>
2025-08-12 20:11:38 +0100

    Remove if statement from object constructor
    Not needed as constructor is being called by addBookToLibrary function with 'book' variable that includes 'new' in declaration.

--- src/script.js
+++ src/script.js
@@ -1,10 +1,6 @@
 const bookLibrary = [];
 
 function Book(title, author, pages, status) {
-  if (!new.target) {
-    throw error("You must use the 'new' operator to call the constructor");
-  }
-
   this.title = title;
   this.author = author;
   this.pages = pages;
